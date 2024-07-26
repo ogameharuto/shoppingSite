66ece17e550bbfa9ceefdafafc55c0eb7fc526ae
STD\s20225002 <20225002-ogameharuto@hcs.ac.jp>
2024-07-26 09:35:05 +0900

    php、css移動

--- aa/productStructure.php
+++ aa/productStructure.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head>
-    aaaaaaa
+<head>aaa
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>在庫管理</title>
