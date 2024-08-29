8e517770ce593e8efe20465cb5fea6b0a9fc8b91
STD\s20227069 <20227069-agamasato@hcs.ac.jp>
2024-08-29 12:06:17 +0900

    自分のストア情報しか見れないように修正

--- script/storeManagerMenu.php
+++ script/storeManagerMenu.php
@@ -2,13 +2,17 @@
 require_once('../utilConnDB.php');
 $utilConnDB = new UtilConnDB();
 $pdo = $utilConnDB->connect();
+session_start();
+
+$store = $_SESSION['store'];
+$storeNumber = $store['storeNumber'];
 
 try {
-
     // ストア情報をデータベースから取得
-    $stmt = $pdo->query('SELECT * FROM store');
+    $query = 'SELECT * FROM store WHERE storeNumber = ?';
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([$storeNumber]);
     $stores = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 } catch (PDOException $e) {
     echo 'エラー: ' . $e->getMessage();
     $stores = [];
@@ -27,55 +31,57 @@ try {
     <h1>ストア管理</h1>
     <div class="table-container">
         <table>
-            <tr>
-                <th>ストア番号</th>
-                <th>会社名</th>
-                <th>郵便番号</th>
-                <th>会社所在地</th>
-                <th>代表者名</th>
-                <th>店舗名</th>
-                <th>フリガナ</th>
-                <th>電話番号</th>        
-                <th>メールアドレス</th>
-                <th>店舗の説明</th>
-                <th>追加情報</th>
-                <th>運営責任者</th>
-                <th>連絡先住所</th>
-                <th>連絡先郵便番号</th>
-                <th>連絡先電話番号</th>
-                <th>連絡先メールアドレス</th>
-                <th>パスワード</th>
-                <th>編集</th>
-            </tr>
+            <thead>
+                <tr>
+                    <th>ストア番号</th>
+                    <th>会社名</th>
+                    <th>郵便番号</th>
+                    <th>会社所在地</th>
+                    <th>代表者名</th>
+                    <th>店舗名</th>
+                    <th>フリガナ</th>
+                    <th>電話番号</th>        
+                    <th>メールアドレス</th>
+                    <th>店舗の説明</th>
+                    <th>追加情報</th>
+                    <th>運営責任者</th>
+                    <th>連絡先住所</th>
+                    <th>連絡先郵便番号</th>
+                    <th>連絡先電話番号</th>
+                    <th>連絡先メールアドレス</th>
+                    <th>パスワード</th>
+                    <th>編集</th>
+                </tr>
+            </thead>
             <tbody>
                 <?php if (!empty($stores)): ?>
-                    <?php foreach ($stores as $store): ?>
+                    <?php foreach ($stores as $storeItem): ?>
                         <tr>
-                            <td><?php echo htmlspecialchars($store['storeNumber'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['companyName'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['companyPostalCode'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['companyAddress'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['companyRepresentative'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['storeName'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['furigana'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['telephoneNumber'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['mailAddress'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['storeDescription'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['storeAdditionalInfo'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['operationsManager'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['contactAddress'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['contactPostalCode'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['contactPhoneNumber'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['contactEmailAddress'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td><?php echo htmlspecialchars($store['password'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['storeNumber'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['companyName'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['companyPostalCode'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['companyAddress'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['companyRepresentative'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['storeName'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['furigana'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['telephoneNumber'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['mailAddress'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['storeDescription'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['storeAdditionalInfo'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['operationsManager'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['contactAddress'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['contactPostalCode'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['contactPhoneNumber'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['contactEmailAddress'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($storeItem['password'], ENT_QUOTES, 'UTF-8'); ?></td>
                             <td>
-                                <a href="editStore.php?id=<?php echo htmlspecialchars($store['storeNumber'], ENT_QUOTES, 'UTF-8'); ?>">編集</a>
+                                <a href="editStore.php?id=<?php echo htmlspecialchars($storeItem['storeNumber'], ENT_QUOTES, 'UTF-8'); ?>">編集</a>
                             </td>
                         </tr>
                     <?php endforeach; ?>
                 <?php else: ?>
                     <tr>
-                        <td colspan="1">ストア情報がありません。</td>
+                        <td colspan="17">ストア情報がありません。</td>
                     </tr>
                 <?php endif; ?>
             </tbody>
